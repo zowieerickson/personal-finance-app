c0ddfae689f86ccc1d82d1a1c56fed911cd24cc2
Zowie Erickson <zowie.erickson@gmail.com>
2025-12-09 07:53:48 -0800

    Store payload to be sent to server

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,66 +1,30 @@
+import { useState } from 'react';
 import styles from './LoginPage.module.css'
 
 export default function LoginPage() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
-    // Recursive Example
-    function fn(i: number) {
-        if (i > 3) {
-            console.log("Finished!")
-            return
-        }
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
 
-        console.log(i)
-        fn(i + 1)
-        console.log(`End of call where i = ${i}`)
-        return
+        const payload = {
+            name,
+            email,
+        };
+
+        const response = await fetch("http://localhost:5173/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+
+        console.log(response)
+        const data = await response.json();
+        console.log("Backend response:", data)
     }
-
-    fn(1) 
-    // End of Recursive Example
-
-    // Higher-Order Function Example
-    function copyArrayAndManipulate(arr: number[], instructions) {
-        let output = [];
-
-        for (let i = 0; i < arr.length; i++) {
-            output.push(instructions(arr[i]))
-        }
-
-        return output
-    }
-
-    function multiplyByTwo(number: number) {
-        return number * 2
-    }
-
-    let myArray = [1, 2, 3]
-
-    // copyArrayAndManipulate(myArray, multiplyByTwo)
-    // End of Higher-Order Function Example
-
-    // Closure Example
-    function outer() {
-        let counter = 0;
-        let marksCounter = 7; // not used, will be garbage collected
-        console.log('marksCounter:', marksCounter)
-
-
-        function incrementByOne() {
-            counter++
-            console.log('OG counter', counter)
-        }
-        return incrementByOne
-    }
-
-    // const randomFunc = outer()
-    // const randomFunc2 = outer()
-
-    // randomFunc()
-    // randomFunc()
-    // randomFunc()
-
-    // randomFunc2()
-
 
     return (
         <div className={styles.login}>
@@ -68,16 +32,21 @@ export default function LoginPage() {
             <a href="/">Home</a>
             <a href="/about">About</a>
             <div className={styles.loginBox}>
-                <form>
-                    <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <h2>Sign up</h2>
                     <div>
-                        <label>Email</label>
-                        <input type="text" name="email" required></input>
+                        <label>
+                            Name <input type="text" name="name" value={name} placeholder="Name" onChange={(e) => setName(e.target.value)}></input>
+                        </label>
                     </div>
                     <div>
-                        <label>Password</label>
-                        <input type="password" name="password" required></input>
+                        <label htmlFor="email">
+                            Email <input type="text" name="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+                        </label>
+                        
                     </div>
+
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         </div>
