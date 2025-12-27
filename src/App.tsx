9ac2f2f0ab08cfd75c8a772f65521f52406d098a
Zowie Erickson <zowie.erickson@gmail.com>
2025-12-27 13:31:17 -0800

    chore: delete About page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router";
 import Home from "./pages/Dashboard/Home.tsx";
 import LoginPage from "./pages/LoginPage";
 import SignUp from "./pages/SignUp";
-import AboutPage from "./pages/AboutPage";
 import Transactions from "./pages/Dashboard/Transactions.tsx";
 import Budgets from "./pages/Dashboard/Budgets.tsx";
 import Pots from "./pages/Dashboard/Pots.tsx";
@@ -25,7 +24,6 @@ function App() {
       {/* Auth Pages */}
       <Route path="login" element={<LoginPage />} />
       <Route path="signup" element={<SignUp />} />
-      <Route path="about" element={<AboutPage />} />
     </Routes>
   );
 }
--- src/pages/AboutPage/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutPage() {
-    return (
-        <div>
-            <h1>This is the about page!</h1>
-            <a href="/">Home</a>
-            <a href="/login">Login</a>
-        </div>
-    )
-}
