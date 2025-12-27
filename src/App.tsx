1955eae593be1057bdf5dbeb00343a2b54c45b7e
Zowie Erickson <zowie.erickson@gmail.com>
2025-12-27 10:18:51 -0800

    chore(routing): move routes from main.tsx to App.tsx to keep main.tsx's goal to serve only as the entry point and mount the react app to the DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+
+import Home from "./pages/Home.tsx";
 import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import AboutPage from "./pages/AboutPage";
+import Transactions from "./pages/Transactions.tsx";
+import Budgets from "./pages/Budgets.tsx";
+import Pots from "./pages/Pots.tsx";
+import RecurringBills from "./pages/RecurringBills.tsx";
 
 function App() {
   return (
-    <>
-      <h1>This is the home page!</h1>
-      <a href="/login">Login</a>
-      <a href="/about">About</a>
-    </>
+    <Routes>
+      {/* Dashboard Pages -- Refactor to be protected route */}
+      <Route path="/" element={<Home />} />
+      <Route path="/transactions" element={<Transactions />} />
+      <Route path="/budgets" element={<Budgets />} />
+      <Route path="/pots" element={<Pots />} />
+      <Route path="/recurring-bills" element={<RecurringBills />} />
+
+      {/* Auth Pages */}
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="about" element={<AboutPage />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
-
-import Home from "./pages/Home.tsx";
-import LoginPage from "./pages/LoginPage";
-import SignUp from "./pages/SignUp";
-import AboutPage from "./pages/AboutPage";
 import App from "./App.tsx";
-import Transactions from "./pages/Transactions.tsx";
-import Budgets from "./pages/Budgets.tsx";
-import Pots from "./pages/Pots.tsx";
-import RecurringBills from "./pages/RecurringBills.tsx";
+
+import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        {/* Dashboard Pages -- Refactor to be protected route */}
-        <Route path="/" element={<Home />} />
-        <Route path="/transactions" element={<Transactions />} />
-        <Route path="/budgets" element={<Budgets />} />
-        <Route path="/pots" element={<Pots />} />
-        <Route path="/recurring-bills" element={<RecurringBills />} />
-
-        {/* Auth Pages */}
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="about" element={<AboutPage />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
