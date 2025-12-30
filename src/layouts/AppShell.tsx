9002d26cc4a0d2ca742ee7fa4b97b9ff007222c6
Zowie Erickson <zowie.erickson@gmail.com>
2025-12-30 14:12:46 -0800

    style(appshell): add space between navbar and outlet

--- src/layouts/AppShell.tsx
+++ src/layouts/AppShell.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 
 export default function AppShell() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen gap-10">
       <Navbar />
       <Outlet />
     </div>
