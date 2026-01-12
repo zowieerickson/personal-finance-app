cecc34c6900e298885fc28163b1e22096fe05808
Zowie Erickson <zowie.erickson@gmail.com>
2026-01-12 13:54:15 -0800

    styles(AuthButton): add hover and active states

--- src/components/auth/AuthSubmit.tsx
+++ src/components/auth/AuthSubmit.tsx
@@ -6,7 +6,7 @@ export default function AuthSubmit({ children, ...props }: AuthSubmitProps) {
   return (
     <button
       type="submit"
-      className="text-sm font-bold text-white bg-[#201F24] py-4 rounded-md"
+      className="text-sm font-bold text-white bg-[#201F24] py-4 rounded-md transition-colors duration-150 hover:bg-grey-500 active:bg-gray-300"
       {...props}
     >
       {children}
